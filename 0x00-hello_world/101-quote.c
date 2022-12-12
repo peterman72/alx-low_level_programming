b13d8c9d7cda139ab6ed68e4e41dd3df6a9b9949
Peter Adebanji <petermankind72@yahoo.com>
2022-12-12 07:13:30 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - prints exactly and that piece of art is useful
+ * " - Dora Korpar, 2015-10-19
  * Return: Always 0 (Success)
  */
 int main(void)
