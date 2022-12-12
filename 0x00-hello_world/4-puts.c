79bf09ac518cdb542f6d6cdb03c4609979c99796
Peter Adebanji <petermankind72@yahoo.com>
2022-12-12 06:32:46 -0800

    puts character

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - prints exactly "programming is like building a multilingual puzzle
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
