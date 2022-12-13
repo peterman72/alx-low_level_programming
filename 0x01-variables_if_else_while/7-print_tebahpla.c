42a7b8816ee0f6785d64135d2935b6a1835be669
Peter Adebanji <petermankind72@yahoo.com>
2022-12-13 04:27:34 -0800

    lowercase alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabets in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp <= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
