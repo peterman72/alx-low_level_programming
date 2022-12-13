b7d43d2f2265a20cac95760f0e782932ec7bc651
Peter Adebanji <petermankind72@yahoo.com>
2022-12-13 05:47:41 -0800

    combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int dig1, dig2, dig3;
+
+	for (dig1 = 0; dig1 < 8; dig++)
+	{
+		for (dig2 = 1; dig2 < 9; dig2++)
+		{
+			for (dig3 = dig2 + 1; dig3 < 10; dig++)
+			{
+				putchar((dig1 % 10) + '0');
+				putchar((dig2 % 10) + '0');
+				putchar((dig3 % 10) + '0');
+				if (dig1 == 7 && dig2 == 8 && dig3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
