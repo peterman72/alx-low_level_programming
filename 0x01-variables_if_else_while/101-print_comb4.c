27dcbf801909a63fb6f21f134f2ed82fabc0a185
Peter Adebanji <petermankind72@yahoo.com>
2022-12-13 05:50:48 -0800

    combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,7 +9,7 @@ int main()
 
 	for (dig1 = 0; dig1 < 8; dig1++)
 	{
-		for (dig2 = 1; dig2 < 9; dig2++)
+		for (dig2 = dig1 + 1; dig2 < 9; dig2++)
 		{
 			for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 			{
