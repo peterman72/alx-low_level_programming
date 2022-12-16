daf7b6c9529f86d0ca747facae4dcdfaed4bba6d
Peter Adebanji <petermankind72@yahoo.com>
2022-12-16 07:12:00 -0800

    corrected the error

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * mul - multiply two integers
- * @a - int type number
- * @b - int type number
+ * @a: int type number
+ * @b: int type number
  * Return: result of multiplication
  */
 int mul(int a, int b)
