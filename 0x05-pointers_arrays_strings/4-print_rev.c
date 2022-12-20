fed3cffb9a7c45652694a85ac85fb89d40b7d262
Peter Adebanji <petermankind72@yahoo.com>
2022-12-20 06:12:25 -0800

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints strings in reverse
+ * @s: char type pointer
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] = '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
