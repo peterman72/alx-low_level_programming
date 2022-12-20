22fd6309f50acc4289478bd17fb84f828a505984
Peter Adebanji <petermankind72@yahoo.com>
2022-12-20 05:56:07 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char type pointer
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (c);
+}
