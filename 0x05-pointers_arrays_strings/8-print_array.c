66e7dcbb3d22702a67ccbda965b8cadf1b654c3b
Peter Adebanji <petermankind72@yahoo.com>
2022-12-21 06:55:14 -0800

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0, n--; n >= 0; n--, i++)
 	{
 		printf("%d", a[i]);
 
-		if (i > 0)
+		if (n > 0)
 		{
 			printf(", ");
 		}
