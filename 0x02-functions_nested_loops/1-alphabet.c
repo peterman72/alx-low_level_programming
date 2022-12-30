5b6e5e1f6ca11a668c42390f5db8f86026caa005
Peter Adebanji <petermankind72@yahoo.com>
2022-12-30 03:27:47 -0800

    alphabets in lowercase added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabts in lowercase
+ * followed by a new line
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = "a"; i <= "z"; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
