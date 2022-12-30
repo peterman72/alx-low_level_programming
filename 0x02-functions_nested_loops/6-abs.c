e1e55ec8f14b89d0e4d1d290e253926d627821ca
Peter Adebanji <petermankind72@yahoo.com>
2022-12-30 06:45:39 -0800

    absolute value of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @n: int type number
+ * Return: Always 0
+ * Description: value must be integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return(n * -1);
+	else
+		return (n);
+}
